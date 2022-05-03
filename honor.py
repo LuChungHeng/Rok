2a0cecd512ca149531f6ebdc6b4842ee533b8878
ChuanHeng <33141754+LuChungHeng@users.noreply.github.com>
2022-05-03 15:14:44 +0800

    Update honor.py

--- honor.py
+++ honor.py
@@ -13,25 +13,25 @@ class RoK:
         self.img_size = self.hash_size * self.hash_size
         pass
 
-    # 自動領取每日獎賞
-    # def get_awards(self):
-    #     print("啟動應用中:")
-    #     os.chdir(self.nox_path)  # 設定cmd預設路徑
-    #     os.system("nox_adb.exe devices")  # 查詢模擬器IP
-    #     os.system("adb -s %s shell am start -n com.lilithgame.roc.gp.tw/com.harry.engine.MainActivity" % self.device_ip)  # 啟動遊戲
-    #     time.sleep(25)
-    #     os.system("adb -s %s shell input swipe 182 76 182 76" % self.device_ip)  # 開啟vip表單
-    #     time.sleep(3)
-    #     os.system("adb -s %s shell input swipe 173 498 173 498" % self.device_ip)  # 領取獎勵
-    #     time.sleep(3)
-    #     os.system("adb -s %s shell input swipe 173 498 173 498" % self.device_ip)  # 完成領取獎勵
-    #     time.sleep(3)
-    #     os.system("adb -s %s shell input swipe 270 213 270 213" % self.device_ip)  # 領取每日vip點數
-    #     time.sleep(3)
-    #     os.system("adb -s %s shell input swipe 270 213 270 213" % self.device_ip)  # 完成每日領取vip點數
-    #     time.sleep(3)
-    #     os.system("adb -s %s shell input swipe 1363 100 1363 100" % self.device_ip)  # 關閉vip表單
-    #     time.sleep(3)
+    自動領取每日獎賞
+    def get_awards(self):
+        print("啟動應用中:")
+        os.chdir(self.nox_path)  # 設定cmd預設路徑
+        os.system("nox_adb.exe devices")  # 查詢模擬器IP
+        os.system("adb -s %s shell am start -n com.lilithgame.roc.gp.tw/com.harry.engine.MainActivity" % self.device_ip)  # 啟動遊戲
+        time.sleep(25)
+        os.system("adb -s %s shell input swipe 182 76 182 76" % self.device_ip)  # 開啟vip表單
+        time.sleep(3)
+        os.system("adb -s %s shell input swipe 173 498 173 498" % self.device_ip)  # 領取獎勵
+        time.sleep(3)
+        os.system("adb -s %s shell input swipe 173 498 173 498" % self.device_ip)  # 完成領取獎勵
+        time.sleep(3)
+        os.system("adb -s %s shell input swipe 270 213 270 213" % self.device_ip)  # 領取每日vip點數
+        time.sleep(3)
+        os.system("adb -s %s shell input swipe 270 213 270 213" % self.device_ip)  # 完成每日領取vip點數
+        time.sleep(3)
+        os.system("adb -s %s shell input swipe 1363 100 1363 100" % self.device_ip)  # 關閉vip表單
+        time.sleep(3)
 
     def screen_shot(self):
         print("啟動應用中:")
@@ -73,5 +73,5 @@ class RoK:
 
 
 obj = RoK()
-# obj.get_awards()
+obj.get_awards()
 obj.screen_shot()
